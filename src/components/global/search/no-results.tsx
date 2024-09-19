a1a690df2ec24f71407053b1a58cb21ea171538f
hamza walker <hamzawalker.vienna@gmail.com>
2024-09-19 09:36:11 +0200

    feat: add ExploreSlider component with infinite scroll and responsive group display

--- src/components/global/search/no-results.tsx
+++ src/components/global/search/no-results.tsx
@@ -12,7 +12,7 @@ export const NoResult = () => {
       <Empty />
       <div>
         <p className="text-xl font-semibold text-themeTextGray">
-          Hmm... Its quite in here
+          Hmm... Its quite Empty in here
         </p>
         <p className="text-sm text-themeTextGray">0 Results Found...</p>
       </div>
