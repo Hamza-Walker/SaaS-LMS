b1e85400af68384552c3f05fba9d2c50257a34b0
hamza walker <hamzawalker.vienna@gmail.com>
2024-09-21 10:35:43 +0200

    feat: add media removal and replacement functionality to group gallery using toast notifications

--- src/app/(discover)/about/_components/gallery.tsx
+++ src/app/(discover)/about/_components/gallery.tsx
@@ -6,104 +6,104 @@ import { BadgePlus } from "@/icons"
 import { validateURLString } from "@/lib/utils"
 import { toast } from "sonner"
 type Props = {
-	gallery: string[]
-	groupid: string
+  gallery: string[]
+  groupid: string
 
-	onActive(media: { url: string | undefined; type: string }): void
-	userid: string
-	groupUserid: string
+  onActive(media: { url: string | undefined; type: string }): void
+  userid: string
+  groupUserid: string
 }
 const MediaGallery = ({
-	gallery,
-	groupUserid,
-	onActive,
-	groupid,
-	userid,
+  gallery,
+  groupUserid,
+  onActive,
+  groupid,
+  userid,
 }: Props) => {
-	const { removeGalleryItem, isPending } = useMediaGallery(groupid)
-	const handleRemove = (mediaUrl: string) => {
-		toast(
-			<div>
-				<p>Are you sure you want to remove this item?</p>
-				<div className="mt-2 flex gap-2 justify-end">
-					<button
-						className="px-4 py-2 text-white bg-red-600 rounded"
-						onClick={() => {
-							removeGalleryItem(mediaUrl) // Call the remove function
-							toast.dismiss() // Close the toast
-						}}
-					>
-						Remove
-					</button>
-					<button
-						className="px-4 py-2 text-white bg-gray-500 rounded"
-						onClick={() => toast.dismiss()}
-					>
-						Cancel
-					</button>
-				</div>
-			</div>,
-			{ duration: Infinity }, // Keep the toast open until the user interacts
-		)
-	}
-	return (
-		<div className="flex justify-start gap-3 flex-wrap">
-			{gallery.length > 0 &&
-				gallery.map((gal, key) => (
-					<div key={key} className="relative w-36 aspect-video">
-						{/* Image Render */}
-						{validateURLString(gal).type === "IMAGE" ? (
-							<img
-								onClick={() =>
-									onActive({
-										url: gal,
-										type: "IMAGE",
-									})
-								}
-								src={`https://ucarecdn.com/${gal}/`}
-								alt="gallery-img"
-								className="rounded-xl cursor-pointer opacity-70 w-full h-full"
-							/>
-						) : (
-							// Video Render (Loom / YouTube)
-							<iframe
-								src={gal}
-								className="absolute outline-none border-0 top-0 left-0 w-full h-full rounded-xl"
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-								allowFullScreen
-							></iframe>
-						)}
+  const { removeGalleryItem, isPending } = useMediaGallery(groupid)
+  const handleRemove = (mediaUrl: string) => {
+    toast(
+      <div>
+        <p>Are you sure you want to remove this item?</p>
+        <div className="mt-2 flex gap-2 justify-end">
+          <button
+            className="px-4 py-2 text-white bg-red-600 rounded"
+            onClick={() => {
+              removeGalleryItem(mediaUrl) // Call the remove function
+              toast.dismiss() // Close the toast
+            }}
+          >
+            Remove
+          </button>
+          <button
+            className="px-4 py-2 text-white bg-gray-500 rounded"
+            onClick={() => toast.dismiss()}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>,
+      { duration: Infinity }, // Keep the toast open until the user interacts
+    )
+  }
+  return (
+    <div className="flex justify-start gap-3 flex-wrap">
+      {gallery.length > 0 &&
+        gallery.map((gal, key) => (
+          <div key={key} className="relative w-36 aspect-video">
+            {/* Image Render */}
+            {validateURLString(gal).type === "IMAGE" ? (
+              <img
+                onClick={() =>
+                  onActive({
+                    url: gal,
+                    type: "IMAGE",
+                  })
+                }
+                src={`https://ucarecdn.com/${gal}/`}
+                alt="gallery-img"
+                className="rounded-xl cursor-pointer opacity-70 w-full h-full"
+              />
+            ) : (
+              // Video Render (Loom / YouTube)
+              <iframe
+                src={gal}
+                className="absolute outline-none border-0 top-0 left-0 w-full h-full rounded-xl"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            )}
 
-						{/* Remove Button */}
-						{userid === groupUserid && (
-							<button
-								className="absolute top-2 right-2 bg-red-500 text-white p-1 rounded-full"
-								onClick={() => handleRemove(gal)}
-								disabled={isPending}
-							>
-								Remove
-							</button>
-						)}
-					</div>
-				))}
+            {/* Remove Button */}
+            {userid === groupUserid && (
+              <button
+                className="absolute  bg-red-500 text-white p-1 rounded-full"
+                onClick={() => handleRemove(gal)}
+                disabled={isPending}
+              >
+                Remove
+              </button>
+            )}
+          </div>
+        ))}
 
-			{userid === groupUserid ? (
-				<GlassModal
-					title="Add media to VSL"
-					description="Paste a link to a youtube or a loom video."
-					trigger={
-						<Card className="border-dashed border-themeGray hover:bg-themeBlack bg-transparent cursor-pointer">
-							<CardContent className="flex justify-center items-center py-10 px-16">
-								<BadgePlus />
-							</CardContent>
-						</Card>
-					}
-				>
-					<MediaGalleryForm groupid={groupid} />
-				</GlassModal>
-			) : null}
-		</div>
-	)
+      {userid === groupUserid ? (
+        <GlassModal
+          title="Add media to VSL"
+          description="Paste a link to a youtube or a loom video."
+          trigger={
+            <Card className="border-dashed border-themeGray hover:bg-themeBlack bg-transparent cursor-pointer">
+              <CardContent className="flex justify-center items-center py-10 px-16">
+                <BadgePlus />
+              </CardContent>
+            </Card>
+          }
+        >
+          <MediaGalleryForm groupid={groupid} />
+        </GlassModal>
+      ) : null}
+    </div>
+  )
 }
 
 export default MediaGallery
